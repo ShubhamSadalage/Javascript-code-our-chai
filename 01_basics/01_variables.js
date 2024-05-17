2996e89971e9888f92cc9a01c8f0c23ce055816e
ShubhamSadalage <88818454+ShubhamSadalage@users.noreply.github.com>
2024-05-17 05:15:14 +0000

    variables & datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =144553
+let accountEmail ="shubam@gmail.com"
+var accountPassward ="12345"
+accountCity="Kolhapur"
+
+//accountId=2
+
+console.log(accountId);
+
+accountEmail="shs@gmail.com"
+accountPassward="1212121"
+accountCity="Pune"
+
+console.table([accountId.accountEmail,accountPassward,accountCity])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,27 @@
+"use srict"; // treat all js code as newer version
+//alert(3+3);  // we are using ndejs not browser
+console.log(3+
+    3) //code redability should be high
+console.log("shubham")
+ 
+//ECMA
+
+let name="shubham"
+let age =18
+let isloggedIn=false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null
+// undefined =>
+// symbol => unique
+
+
+///object
+
+console.log(typeof "shubham")
+console.log(typeof age)
+console.log(typeof null)  //object
+console.log(typeof undefined) //undefined
